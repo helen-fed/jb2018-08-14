b908048e10105c99b02635b19ed327e587aa2fda
helen-fed <fedotovah@gmail.com>
2018-08-18 13:26:01 +0300

    Hello World from master branch!

--- src/com/example/helloWorld/HelloWorld.java
+++ src/com/example/helloWorld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World from master branch!");
+        System.out.println("Hello World from master branch!");
         System.out.println("Hello World from dev brancn!");
         System.out.println("Hello World from feature brancn!");
         System.out.println("Hello World from master branch!");
