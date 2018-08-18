4a7909dca14ee6ae5ff648abf3256798a24a6ec1
helen-fed <fedotovah@gmail.com>
2018-08-18 21:24:39 +0300

    Hello World from dev brancn!

--- src/com/example/helloWorld/HelloWorld.java
+++ src/com/example/helloWorld/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Hello World from master branch!");
         System.out.println("Hello World from dev brancn!");
+        System.out.println("Hello World from feature brancn!");
     }
 }
