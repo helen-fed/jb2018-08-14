001524a810e4fdf1d03fd210a298557a7f5b21fd
helen-fed <fedotovah@gmail.com>
2018-08-18 14:00:39 +0300

    Hello World from dev brancn!

--- src/com/example/helloWorld/HelloWorld.java
+++ src/com/example/helloWorld/HelloWorld.java
@@ -3,5 +3,6 @@ package com.example.helloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World from dev brancn!");
     }
 }
